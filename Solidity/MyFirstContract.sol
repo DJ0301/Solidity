6c4e76fa983fc79e3e83029f659512f7b0db3a01
DJ0301 <dhananjayjoshi2003@gmail.com>
2023-04-08 00:25:07 +0530

    .

---
+++ Solidity/MyFirstContract.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.9;
+contract mycontract
+{
+    string test = "Hello Solidity";
+    constructor(string memory test1)
+    {
+        test1 = test ;
+    }
+}
---
+++ Solidity/hardhat_1.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.12;
+contract HelloWorld {
+
+  event messagechanged(string oldmsg, string newmsg);
+
+  string public message;
+
+  constructor(string memory firstmessage) {
+
+    message = firstmessage;
+
+  }
+
+  function update(string memory newmesssage) public {
+
+    string memory oldmsg = message;
+
+    message = newmesssage;
+
+    emit messagechanged(oldmsg, newmesssage);
+
+  }
+}
